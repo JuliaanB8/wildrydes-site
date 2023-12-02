b8c6390a8f462a06285e80e23deaabec31f380b0
Julian Buendia <flores.julian60@gmail.com>
2023-12-02 15:25:30 -0600

    moved function

--- js/ride.js
+++ js/ride.js
@@ -4,7 +4,6 @@ var WildRydes = window.WildRydes || {};
 WildRydes.map = WildRydes.map || {};
 let map;
 
-updateWeather("Fort Worth", "80", "1021", "62", "15mph");
 
 (function rideScopeWrapper($) {
     var authToken;
@@ -19,7 +18,7 @@ updateWeather("Fort Worth", "80", "1021", "62", "15mph");
         window.location.href = '/signin.html';
     });
     
-
+    
     //  requestUnicorn
     //      make the POST request to the server
     function requestUnicorn(pickupLocation) {
@@ -44,149 +43,153 @@ updateWeather("Fort Worth", "80", "1021", "62", "15mph");
             }
         });
     }
-
+    
     //  completeRequest
     //      a Unicorn has been dispatched to your location
     function completeRequest(result, pickupLocation) {
         var unicorn;
         var pronoun;
-
+        
         console.log('Response received from API: ', result);
         unicorn = result.Unicorn;
         pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
         displayUpdate(unicorn.Name + ', your ' + unicorn.Color + ' unicorn, is on ' + pronoun + ' way.', unicorn.Color);
-
+        
         console.log(pickupLocation);
         //  get the local weather, find nearby restaurants, movies
         //getWeather(pickupLocation, unicorn);
-
+        
         animateArrival(function animateCallback() {
             displayUpdate(unicorn.Name + ' has arrived. Giddy up!', unicorn.Color);
             WildRydes.map.unsetLocation();
-
-
+            
+            
             $('#request').prop('disabled', 'disabled');
             $('#request').text('Set Pickup');
         });
     }
-
+    
     // Register click handler for #request button
     $(function onDocReady() {
         $('#request').click(handleRequestClick);
-
+        
         WildRydes.authToken.then(function updateAuthMessage(token) {
             if (token) {
                 displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
                 $('.authToken').text(token);
             }
         });
-
+        
         if (!_config.api.invokeUrl) {
             $('#noApiMessage').show();
         }
-
+        
         window.navigator.geolocation
-            .getCurrentPosition(setLocation);
-
+        .getCurrentPosition(setLocation);
+        
         //  put the map behind the updates list
         document.getElementById("map").style.zIndex = "10";
-
+        
         function setLocation(loc) {
             map = L.map('map').setView([loc.coords.latitude, loc.coords.longitude], 13);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '© OpenStreetMap'
             }).addTo(map);
-
+            
             WildRydes.map.center = {latitude: loc.coords.latitude, longitude: loc.coords.longitude};
             let b = map.getBounds();        //  TODO moved
             WildRydes.map.extent = {minLat: b._northEast.lat, minLng: b._northEast.lng,
                 maxLat: b._southWest.lat, maxLng: b._southWest.lng};
-
-            WildRydes.marker  = L.marker([loc.coords.latitude, loc.coords.longitude]).addTo(map);
-            var myIcon = L.icon({
-                iconUrl: 'images/unicorn-icon.png',
-                iconSize: [25, 25],
-                iconAnchor: [22, 24],
-                shadowSize: [25, 25],
-                shadowAnchor: [22, 24]
-            });
-            WildRydes.unicorn = L.marker([loc.coords.latitude, loc.coords.longitude], {icon: myIcon}).addTo(map);
-            // WildRydes.marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-            // var popup = L.popup();
-            map.on('click', onMapClick);
-
-            function onMapClick(e) {            //  TODO move to esri.js
-                WildRydes.map.selectedPoint = {longitude: e.latlng.lng, latitude: e.latlng.lat};
-                if (WildRydes.marker)       WildRydes.marker.remove();
-                handlePickupChanged();
-
-                WildRydes.marker  = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-
-                // popup
-                //     .setLatLng(e.latlng)
-                //     .setContent("You clicked the map at " + e.latlng.toString())
-                //     .openOn(map);
+                
+                WildRydes.marker  = L.marker([loc.coords.latitude, loc.coords.longitude]).addTo(map);
+                var myIcon = L.icon({
+                    iconUrl: 'images/unicorn-icon.png',
+                    iconSize: [25, 25],
+                    iconAnchor: [22, 24],
+                    shadowSize: [25, 25],
+                    shadowAnchor: [22, 24]
+                });
+                WildRydes.unicorn = L.marker([loc.coords.latitude, loc.coords.longitude], {icon: myIcon}).addTo(map);
+                // WildRydes.marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+                
+                // var popup = L.popup();
+                map.on('click', onMapClick);
+                
+                function onMapClick(e) {            //  TODO move to esri.js
+                    WildRydes.map.selectedPoint = {longitude: e.latlng.lng, latitude: e.latlng.lat};
+                    if (WildRydes.marker)       WildRydes.marker.remove();
+                    handlePickupChanged();
+                    
+                    WildRydes.marker  = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+                    
+                    // popup
+                    //     .setLatLng(e.latlng)
+                    //     .setContent("You clicked the map at " + e.latlng.toString())
+                    //     .openOn(map);
+                }
             }
+        });
+        
+        //  handlePickupChanged
+        //      enable the Pickup button and set text to Request Unicorn
+        function handlePickupChanged() {
+            var requestButton = $('#request');
+            requestButton.text('Request Unicorn');
+            requestButton.prop('disabled', false);
         }
-    });
-
-    //  handlePickupChanged
-    //      enable the Pickup button and set text to Request Unicorn
-    function handlePickupChanged() {
-        var requestButton = $('#request');
-        requestButton.text('Request Unicorn');
-        requestButton.prop('disabled', false);
-    }
-
-    //  handleRequestClick
-    //      get current request location and POST request to server
-    function handleRequestClick(event) {
-        var pickupLocation =  WildRydes.map.selectedPoint;
-
-        event.preventDefault();
-        requestUnicorn(pickupLocation);
-    }
-
-    //  animateArrival
-    //      animate the Unicorn's arrival to the user's pickup location
-    function animateArrival(callback) {
-        var dest = WildRydes.map.selectedPoint;
-        var origin = {};
-
-        if (dest.latitude > WildRydes.map.center.latitude) {
-            origin.latitude = WildRydes.map.extent.minLat;
-        } else {
-            origin.latitude = WildRydes.map.extent.maxLat;
+        
+        //  handleRequestClick
+        //      get current request location and POST request to server
+        function handleRequestClick(event) {
+            var pickupLocation =  WildRydes.map.selectedPoint;
+            
+            event.preventDefault();
+            requestUnicorn(pickupLocation);
         }
-
-        if (dest.longitude > WildRydes.map.center.longitude) {
-            origin.longitude = WildRydes.map.extent.minLng;
-        } else {
-            origin.longitude = WildRydes.map.extent.maxLng;
+        
+        //  animateArrival
+        //      animate the Unicorn's arrival to the user's pickup location
+        function animateArrival(callback) {
+            var dest = WildRydes.map.selectedPoint;
+            var origin = {};
+            
+            if (dest.latitude > WildRydes.map.center.latitude) {
+                origin.latitude = WildRydes.map.extent.minLat;
+            } else {
+                origin.latitude = WildRydes.map.extent.maxLat;
+            }
+            
+            if (dest.longitude > WildRydes.map.center.longitude) {
+                origin.longitude = WildRydes.map.extent.minLng;
+            } else {
+                origin.longitude = WildRydes.map.extent.maxLng;
+            }
+            
+            WildRydes.map.animate(origin, dest, callback);
         }
-
-        WildRydes.map.animate(origin, dest, callback);
+        
+        updateWeather("Fort Worth", "80", "1021", "62", "15mph");
+        
+    }(jQuery));
+    
+    //  these functions below here are my utility functions
+    //      to present messages to users
+    //      and to particularly add some 'sizzle' to the application
+    
+    //  displayUpdate
+    //      nice utility method to show message to user
+    function displayUpdate(text, color='green') {
+        $('#updates').prepend($(`<li style="background-color:${color}">${text}</li>`));
     }
-
-
-}(jQuery));
-
-//  these functions below here are my utility functions
-//      to present messages to users
-//      and to particularly add some 'sizzle' to the application
-
-//  displayUpdate
-//      nice utility method to show message to user
-function displayUpdate(text, color='green') {
-    $('#updates').prepend($(`<li style="background-color:${color}">${text}</li>`));
-}
-function updateWeather(city, temperature, pressure, humidity, windSpeed){
-    $('#city').text('City:' + city);
-    $('#temp').text('Temperature:' + temperature);
-    $('#press').text('Pressure:' + pressure);
-    $('#humidity').text('Humidity:' + humidity);
-    $('#wind').text('Wind Speed:' + windSpeed);
-}
-
+    
+    // UpdateWeather
+    function updateWeather(city, temperature, pressure, humidity, windSpeed){
+        $('#city').text('City:' + city);
+        $('#temp').text('Temperature:' + temperature);
+        $('#press').text('Pressure:' + pressure);
+        $('#humidity').text('Humidity:' + humidity);
+        $('#wind').text('Wind Speed:' + windSpeed);
+    }
+    
+    
